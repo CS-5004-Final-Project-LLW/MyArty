470a3c2295f7803c7d98e3ef23109dc92526f06c
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-04-26 00:50:20 -0700

    feat: largen image

---
+++ Coordinate/CoordinateAndSize.java
@@ -0,0 +1,13 @@
+package Coordinate;
+
+public class CoordinateAndSize {
+    public int x, y, width, height;
+
+    public CoordinateAndSize(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+}
